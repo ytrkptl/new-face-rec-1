188f5b11b4d194f45fe81559201dcadcc052d2e8
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2019-09-04 11:06:05 -0400

    updated links in Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,14 @@ const Footer = () => {
 		<footer className="FooterClass">
 			<h3 className="FooterTitle" >Credits/Links</h3>
 			<div className="FooterGrid">
-					<a className="FooterLinkStyle" href="http://clipart-library.com/clipart/114791.htm">Thunderstorm Logo</a>
-					<a className="FooterLinkStyle" href="/">My Portfolio</a>
-					<a className="FooterLinkStyle" href="/">GitHub</a>
-					<a className="FooterLinkStyle" href="/">LinkedIn</a>
+					<a className="FooterLinkStyle" href="http://clipart-library.com/clipart/114791.htm" target="_blank" rel="noopener noreferrer">Thunderstorm Logo</a>
+					<a className="FooterLinkStyle" href="https://emojis.wiki/emoji-pics/messenger/droplet-messenger.png" target="_blank" rel="noopener noreferrer">Rain droplet</a>
+					<a className="FooterLinkStyle" href="http://www.transparentpng.com/details/photo-editing-effects-thunderstorm-_1558.html" target="_blank" rel="noopener noreferrer">Lightning Image</a>
+					<a className="FooterLinkStyle" href="https://codepen.io/Chrislion_me/pen/rVqwbO" target="_blank" rel="noopener noreferrer">Storm Lightning Codepen</a>
+					<a className="FooterLinkStyle" href="http://soundbible.com/2053-Thunder-Sound-FX.html" target="_blank" rel="noopener noreferrer">Thunder Sound FX Sound</a>					
+					<a className="FooterLinkStyle" href="https://www.yatrik.dev" target="_blank" rel="noopener noreferrer">My Portfolio</a>
+					<a className="FooterLinkStyle" href="https://github.com/ytrkptl" target="_blank" rel="noopener noreferrer">GitHub</a>
+					<a className="FooterLinkStyle" href="https://www.linkedin.com/in/yatrik-patel-7351b5170/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
 			</div>
 			<hr/>
 			<p className="FooterAuthor" >Created by: Yatrik Patel, September 4, 2019</p>
